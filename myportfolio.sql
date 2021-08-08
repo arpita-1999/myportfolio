a1a1002d991e365812224788960080c804b6c3c1
arpita-1999 <arpita.maharatha1999@gmail.com>
2021-08-08 09:54:15 +0530

    Add files via upload

---
+++ myportfolio.sql
@@ -0,0 +1,434 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2021 at 05:35 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myportfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `about_title` varchar(255) NOT NULL,
+  `about_subtitle` text NOT NULL,
+  `profile_pic` text NOT NULL,
+  `about_desc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `about_title`, `about_subtitle`, `profile_pic`, `about_desc`) VALUES
+(1, 'JAVA Developer', 'I work independently as freelancers or with company teams to create websites. Depending on the job, I may focus on front-end development, which involves designing sites and producing content, or back-end development, which involves writing code to make website features work.', '1627659119dp0.jpg', 'This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me. This is a description about me.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `admin_profile` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `fullname`, `admin_profile`, `email`, `password`) VALUES
+(1, 'New Admin', '1627657631user3.png', 'admin12345@gmail.com', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `address` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `address`, `email`, `mobile`) VALUES
+(1, 'naddress', 'nmail@com', '9876543210');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home`
+--
+
+CREATE TABLE `home` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) NOT NULL,
+  `showicons` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `home`
+--
+
+INSERT INTO `home` (`id`, `title`, `subtitle`, `showicons`) VALUES
+(1, 'Arpita Maharatha', 'I\'m a passionate Web Developer from India', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_info`
+--
+
+CREATE TABLE `personal_info` (
+  `id` int(11) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `personal_info`
+--
+
+INSERT INTO `personal_info` (`id`, `label`, `value`) VALUES
+(1, 'Website', 'www.doalong.com'),
+(4, 'Email-id', 'newadmin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `project_pic` text NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `project_type` varchar(255) NOT NULL,
+  `project_link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `project_pic`, `project_name`, `project_type`, `project_link`) VALUES
+(1, 'portfolio-1.jpg', 'Software Mongo', 'APP', 'https://studio.youtube.com/video/MwBQy53OoDY/edit'),
+(3, '1627645680corona.jpg', 'CovidTracker', 'WEBSITE', 'https://www.hostinger.in/'),
+(4, '1627646013portfolio-3.jpg', 'Xcromo', 'SOFTWARE', 'xcromo.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resume`
+--
+
+CREATE TABLE `resume` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `org` varchar(255) NOT NULL,
+  `about_exp` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resume`
+--
+
+INSERT INTO `resume` (`id`, `type`, `title`, `time`, `org`, `about_exp`) VALUES
+(1, 'e', 'Bachelors of Science', '2017-2020', 'IGNOU', 'Awesome Experience'),
+(5, 'p', 'Web Developer', '2017-Present', 'Doalongme Pvt. Ltd.', 'Awesome Experience');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section_control`
+--
+
+CREATE TABLE `section_control` (
+  `id` int(11) NOT NULL,
+  `home_section` varchar(255) NOT NULL,
+  `about_section` varchar(255) NOT NULL,
+  `resume_section` varchar(255) NOT NULL,
+  `services_section` varchar(255) NOT NULL,
+  `portfolio_section` varchar(255) NOT NULL,
+  `contact_section` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `section_control`
+--
+
+INSERT INTO `section_control` (`id`, `home_section`, `about_section`, `resume_section`, `services_section`, `portfolio_section`, `contact_section`) VALUES
+(1, 'on', 'on', 'on', '0', 'on', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seo`
+--
+
+CREATE TABLE `seo` (
+  `id` int(11) NOT NULL,
+  `page_title` text NOT NULL,
+  `keywords` text NOT NULL,
+  `description` text NOT NULL,
+  `siteicon` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `seo`
+--
+
+INSERT INTO `seo` (`id`, `page_title`, `keywords`, `description`, `siteicon`) VALUES
+(1, 'Arpita Portfolio', 'Web Developer, JAVA Developer', 'I\'m an ethusiastic learner.', '945b0225337909.563440870421b.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_background`
+--
+
+CREATE TABLE `site_background` (
+  `id` int(11) NOT NULL,
+  `background_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `site_background`
+--
+
+INSERT INTO `site_background` (`id`, `background_img`) VALUES
+(1, '1627659344bg.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill`
+--
+
+CREATE TABLE `skill` (
+  `id` int(11) NOT NULL,
+  `skill_name` varchar(255) NOT NULL,
+  `skill_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `skill`
+--
+
+INSERT INTO `skill` (`id`, `skill_name`, `skill_level`) VALUES
+(1, 'HTML', 100),
+(2, 'CSS', 80),
+(4, 'Bootstrap', 50),
+(5, 'JAVA', 20),
+(6, 'Python', 71);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_media`
+--
+
+CREATE TABLE `social_media` (
+  `id` int(11) NOT NULL,
+  `twitter` varchar(255) NOT NULL,
+  `facebook` varchar(255) NOT NULL,
+  `instagram` varchar(255) NOT NULL,
+  `skype` varchar(255) NOT NULL,
+  `linkedin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `social_media`
+--
+
+INSERT INTO `social_media` (`id`, `twitter`, `facebook`, `instagram`, `skype`, `linkedin`) VALUES
+(1, 'ntwitter', 'nfb', 'ninsta', 'nskype', 'nlinkedin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `home`
+--
+ALTER TABLE `home`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `resume`
+--
+ALTER TABLE `resume`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `section_control`
+--
+ALTER TABLE `section_control`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seo`
+--
+ALTER TABLE `seo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `site_background`
+--
+ALTER TABLE `site_background`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `skill`
+--
+ALTER TABLE `skill`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `social_media`
+--
+ALTER TABLE `social_media`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `home`
+--
+ALTER TABLE `home`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `resume`
+--
+ALTER TABLE `resume`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `section_control`
+--
+ALTER TABLE `section_control`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `seo`
+--
+ALTER TABLE `seo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `site_background`
+--
+ALTER TABLE `site_background`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `skill`
+--
+ALTER TABLE `skill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `social_media`
+--
+ALTER TABLE `social_media`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
